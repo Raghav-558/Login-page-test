308be5c4914729eab4d12e48b88ef5b0316d39de
Raghav-558 <jangraraghav9211@gmail.com>
2025-02-12 12:23:44 +0530

    change form validation

--- src/components/home/LoginForm.tsx
+++ src/components/home/LoginForm.tsx
@@ -7,10 +7,16 @@ import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [remember, setRemember] = useState(false);
-  const [error, setError] = useState("");
+  const form = {
+    email: "",
+    password: "",
+    checkbox: false,
+  };
+
+  const [formData, setFormData] = useState(form);
+  const [error, setError] = useState(false);
+  const [checkboxError, setCheckboxError] = useState(false);
+  const emailSyntax = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   useEffect(() => {
     const isAuthenticated = localStorage.getItem("isAuthenticated");
@@ -21,37 +27,19 @@ const LoginForm = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    setError("");
+    setError(true);
+    setCheckboxError(!formData.checkbox);
 
-    const emailSyntax = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (!email && !password) {
-      setError("Email and password are required");
-      return;
+    if (
+      (formData.email && emailSyntax.test(formData.email)) ||
+      (!emailSyntax && formData.password.length >= 6)
+    ) {
+      setFormData(form);
+      setError(false);
+      setCheckboxError(false);
+      localStorage.setItem("isAuthenticated", "true");
+      router.push("/dashboard");
     }
-
-    if (!emailSyntax.test(email)) {
-      setError("Invalid email format");
-      return;
-    }
-    
-    if (password.length === 0) {
-      setError("password is required");
-      return;
-    }
-
-    if (password.length < 6) {
-      setError("Password must have a minimum of 6 characters");
-      return;
-    }
-
-    if (!remember) {
-      setError("You must agree to remember for 30 days");
-      return;
-    }
-
-    localStorage.setItem("isAuthenticated", "true");
-    router.push("/dashboard");
   };
 
   return (
@@ -66,13 +54,13 @@ const LoginForm = () => {
               height={61.71}
               className="pointer-events-none pb-[138.9px] max-md:pb-[90px]"
             />
+            <h2 className="font-semibold text-3xl leading-[58.45px] text-custom-black">
+              Welcome Back
+            </h2>
+            <p className="text-sm leading-[30px] text-custom-gray pb-[31px]">
+              Welcome back! Please enter your details.
+            </p>
             <form onSubmit={handleSubmit} noValidate className="max-w-[456px]">
-              <h2 className="font-semibold text-3xl leading-[58.45px] text-custom-black">
-                Welcome Back
-              </h2>
-              <p className="text-sm leading-[30px] text-custom-gray pb-[31px]">
-                Welcome back! Please enter your details.
-              </p>
               <div className="pb-[18px]">
                 <label
                   htmlFor="email"
@@ -83,11 +71,21 @@ const LoginForm = () => {
                 <input
                   type="email"
                   id="email"
-                  value={email}
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
                   placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
                   className="px-[14px] py-[19.2px] outline-none w-[456px] text-light-gray rounded-lg border border-custom-white max-md:w-[320px] shadow-[0_1px_2px_0_#1018280D]"
                 />
+                {error && formData.email.length === 0 ? (
+                  <p className="text-red-600 pt-2">Email is required</p>
+                ) : !emailSyntax.test(formData.email) &&
+                  formData.email.length > 0 ? (
+                  <p className="text-red-600 pt-2">Enter valid email</p>
+                ) : (
+                  ""
+                )}
               </div>
               <div>
                 <label
@@ -99,11 +97,23 @@ const LoginForm = () => {
                 <input
                   type="password"
                   id="password"
-                  value={password}
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
                   placeholder="••••••••"
-                  onChange={(e) => setPassword(e.target.value)}
                   className="px-[14px] py-[19.2px] outline-none w-[456px] text-light-gray rounded-lg border border-custom-white max-md:w-[320px] shadow-[0_1px_2px_0_#1018280D]"
                 />
+                {error && formData.password.length === 0 ? (
+                  <p className="text-red-600 pt-2">Password is required</p>
+                ) : formData.password.length < 6 &&
+                  formData.password.length > 0 ? (
+                  <p className="text-red-600 pt-2">
+                    Password must be 6 characters
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
               <div className="flex md:items-center justify-between pt-[18px] max-md:flex-col max-md:gap-[14px]">
                 <label
@@ -113,8 +123,10 @@ const LoginForm = () => {
                   <input
                     type="checkbox"
                     id="remember"
-                    checked={remember}
-                    onChange={(e) => setRemember(e.target.checked)}
+                    checked={formData.checkbox}
+                    onChange={(e) =>
+                      setFormData({ ...formData, checkbox: e.target.checked })
+                    }
                     className="!size-5 !bg-white !rounded-md !border !border-solid !border-custom-white"
                   />
                   <span className="inter leading-6 text-very-light-gray">
@@ -128,24 +140,26 @@ const LoginForm = () => {
                   Forgot password
                 </Link>
               </div>
-              {error && <p className="text-red-500 pt-6">{error}</p>}
+              {checkboxError && (
+                <p className="text-red-600 pt-2">You must agree to the terms</p>
+              )}
               <button
                 type="submit"
                 className="pt-[9px] pb-[10px] bg-deep-black text-white w-full mt-6 hover:bg-green-500 transition-all duration-300 rounded-[9px]"
               >
                 Sign In
               </button>
-              <div className="pt-[11px] pb-3 cursor-pointer bg-white w-full mt-[6px] rounded-[9px] border border-custom-white flex items-center gap-[10px] justify-center hover:bg-gray-200 transition-all duration-300">
-                <GoogleIcon />
-                <p>Sign in with Google</p>
-              </div>
-              <p className="inter leading-6 text-base md:text-center pt-[18px] text-very-light-gray">
-                Don’t have an account?{" "}
-                <Link href="/" className="text-custom-blue">
-                  Sign up
-                </Link>
-              </p>
             </form>
+            <div className="pt-[11px] pb-3 cursor-pointer bg-white w-full mt-[6px] rounded-[9px] border border-custom-white flex items-center gap-[10px] justify-center hover:bg-gray-200 transition-all duration-300">
+              <GoogleIcon />
+              <p>Sign in with Google</p>
+            </div>
+            <p className="inter leading-6 text-base md:text-center pt-[18px] text-very-light-gray">
+              Don’t have an account?{" "}
+              <Link href="/" className="text-custom-blue">
+                Sign up
+              </Link>
+            </p>
           </div>
           <Image
             src="/assets/images/hero.webp"
